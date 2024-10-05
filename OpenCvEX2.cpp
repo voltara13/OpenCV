fdf6e49b94b6d372886071c20456f29bc2aca70a
v.simonov <v.simonov@ledas.com>
2024-10-05 21:53:45 +0700

    Add ex6

--- OpenCvEX2.cpp
+++ OpenCvEX2.cpp
@@ -1,93 +1,127 @@
 ﻿#include <opencv2/opencv.hpp>
 #include <vector>
-#include <random>
 #include <iostream>
 
+enum class NoiseType {
+  Uniform,
+  Gaussian,
+
+  Count
+};
+
+enum class FilterType {
+  Gaussian,
+  Median,
+  Filter2D,
+  Sobel,
+  Laplace,
+
+  Count
+};
+
+double NOISE_POWER = 0.5;
+
+NoiseType SELECTED_NOISE_TYPE = NoiseType::Uniform;
+FilterType SELECTED_FILTER_TYPE = FilterType::Gaussian;
+
 const char* INPUT_PATH = "../../../input.png";
-const char* WINDOW_SOURCE_NAME = "Source image";
-const char* WINDOW_NOISE_NAME = "Noise image";
-const char* WINDOW_OUTPUT_NAME = "Output image";
+const char* WINDOW_NAME = "Filter image";
 
-constexpr int NOISE_COUNT = 3;
-constexpr double NOISE_POWER = 0.1;
-
-uchar findAvg(const std::vector<uchar>& arr) 
+cv::Mat getNoiseMat(const cv::Size& iSize)
 {
-  auto sum = 0;
+  cv::Mat randomMat(iSize, CV_32F);
 
-  for (const auto& value : arr)
-    sum += value;
-
-  return cv::saturate_cast<uchar>(sum / arr.size());
-}
-
-std::vector<cv::Mat> getNoise(const cv::Mat& iImage)
-{
-  std::vector<cv::Mat> noiseImages;
-
-  const auto rows = iImage.rows;
-  const auto cols = iImage.cols;
-
-  for (int i = 0; i < NOISE_COUNT; ++i)
-    noiseImages.emplace_back(iImage.size(), iImage.type());
-
-  std::random_device dev;
-  std::mt19937 rng(dev());
-  std::uniform_int_distribution<> dist(0, 512);
-
-  for (int row = 0; row < rows; ++row) {
-    for (int col = 0; col < cols; ++col) {
-      for (int i = 0; i < noiseImages.size(); ++i) {
-        noiseImages[i].at<uchar>(row, col) = cv::saturate_cast<uchar>(iImage.at<uchar>(row, col) + (dist(rng) - 255) * NOISE_POWER);
-      }
-    }
+  switch (SELECTED_NOISE_TYPE) {
+    case NoiseType::Uniform:
+      cv::randu(randomMat, -255, 255);
+      break;
+    case NoiseType::Gaussian:
+      constexpr float mean = 0.0;
+      constexpr float stddev = 100;
+      cv::randn(randomMat, mean, stddev);
+      break;
   }
 
-  return noiseImages;
+  return randomMat;
 }
 
-cv::Mat replaceImage(const std::vector<cv::Mat>& iNoises)
+cv::Mat getNoise(const cv::Mat& iImage)
 {
-  if (iNoises.empty())
-    return {};
+  const auto noiseMat = getNoiseMat(iImage.size());
 
-  const auto rows = iNoises[0].rows;
-  const auto cols = iNoises[0].cols;
+  cv::Mat noiseImage;
+  iImage.convertTo(noiseImage, CV_32F);
+  noiseImage = noiseImage + noiseMat * NOISE_POWER;
+  noiseImage.convertTo(noiseImage, iImage.type());
 
-  cv::Mat replaced(rows, cols, CV_8UC1, cv::Scalar(0));
+  return noiseImage;
+}
 
-  for (int row = 0; row < rows; ++row) {
-    for (int col = 0; col < cols; ++col) {
-      std::vector<uchar> pixelsVariant(iNoises.size());
+cv::Mat getFiltered(const cv::Mat& iImage)
+{
+  cv::Mat filteredImage;
 
-      for (int i = 0; i < iNoises.size(); ++i)
-        pixelsVariant[i] = iNoises[i].at<uchar>(row, col);
+  const auto filter2DKernel = cv::Mat(3, 3, CV_32F, 1 / 9.0);
+  const auto sobelDx = 1;
+  const auto sobelDy = 0;
 
-      replaced.at<uchar>(row, col) = findAvg(pixelsVariant);
-    }
+  switch (SELECTED_FILTER_TYPE) {
+    case FilterType::Gaussian:
+      cv::blur(iImage, filteredImage, cv::Size(3, 3));
+      break;
+    case FilterType::Median:
+      cv::medianBlur(iImage, filteredImage, 3);
+      break;
+    case FilterType::Filter2D:
+      cv::filter2D(iImage, filteredImage, -1, filter2DKernel);
+      break;
+    case FilterType::Sobel:
+      cv::Sobel(iImage, filteredImage, -1, sobelDx, sobelDy);
+      break;
+    case FilterType::Laplace:
+      cv::Laplacian(iImage, filteredImage, -1, 5, 10);
+      break;
   }
 
-  return replaced;
+  return filteredImage;
+}
+
+void onTrackbarNoisePower(int iPower, void*) {
+  NOISE_POWER = iPower / 10.0;
+}
+
+void onTrackbarNoise(int iNoise, void*) {
+  SELECTED_NOISE_TYPE = static_cast<NoiseType>(iNoise);
+}
+
+void onTrackbarFilter(int iFilter, void*) {
+  SELECTED_FILTER_TYPE = static_cast<FilterType>(iFilter);
 }
 
 int main(int argc, char** argv)
 {
-  auto image = cv::imread(INPUT_PATH, cv::IMREAD_GRAYSCALE);
-  auto noiseImages = getNoise(image);
-  
-  cv::imshow(WINDOW_SOURCE_NAME, image);
- 
-  for (int i = 0; i < noiseImages.size(); ++i) {
-    auto name = cv::String(WINDOW_NOISE_NAME);
-    name += "_" + std::to_string(i);
+  auto noisePower = 3;
+  auto noiseType = static_cast<int>(SELECTED_NOISE_TYPE);
+  auto filterType = static_cast<int>(SELECTED_FILTER_TYPE);
 
-    cv::imshow(name, noiseImages[i]);
+  const auto image = cv::imread(INPUT_PATH, cv::IMREAD_GRAYSCALE);
+
+  cv::namedWindow(WINDOW_NAME, cv::WINDOW_AUTOSIZE);
+
+  cv::createTrackbar("Power", WINDOW_NAME, &noisePower, 10, onTrackbarNoisePower);
+  cv::createTrackbar("Noise", WINDOW_NAME, &noiseType, static_cast<int>(NoiseType::Count) - 1, onTrackbarNoise);
+  cv::createTrackbar("Filter", WINDOW_NAME, &filterType, static_cast<int>(FilterType::Count) - 1, onTrackbarFilter);
+
+  while (cv::waitKey(30) != 'q') {
+    const auto noiseImage = getNoise(image);
+    const auto filteredImage = getFiltered(noiseImage);
+
+    cv::Mat concatenatedImages;
+    cv::hconcat(std::vector<cv::Mat>{image, noiseImage, filteredImage}, concatenatedImages);
+    cv::imshow(WINDOW_NAME, concatenatedImages);
   }
 
-  const auto replacedImage = replaceImage(noiseImages);
-  cv::imshow(WINDOW_OUTPUT_NAME, replacedImage);
-
-  cv::waitKey();
+  cv::destroyAllWindows();
 
   return EXIT_SUCCESS;
 }
